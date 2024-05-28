8bdcb0fdbb60b2d351c82e8c998098ec79af7833
Harsh <76443685+11harshkumar@users.noreply.github.com>
2024-05-28 22:30:48 +0530

    Create User Router

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,15 @@
 import express from "express";
 import globalErrorHandler from "./middlewares/gloabalErrorHandler";
+import userRouter from "./user/userRouter";
 
 const app = express();
 
 app.get("/", (req, res, next) => {
     res.json({ message: "welcome to elib apis" });
 });
+
+// Registering the user Router
+app.use("/api/users", userRouter);
 
 // Global Error Handler
 app.use(globalErrorHandler);
