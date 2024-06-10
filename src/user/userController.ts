29ff5b47fd10c724b9936eeb586014a02bf4d350
Harsh <76443685+11harshkumar@users.noreply.github.com>
2024-06-10 12:18:24 +0530

    add user model

--- src/user/userController.ts
+++ src/user/userController.ts
@@ -1,12 +1,29 @@
 import { Request, Response, NextFunction } from "express";
 import createHttpError from "http-errors";
+import userModel from "./userModel";
 
 const createUser = async (req: Request, res: Response, next: NextFunction) => {
     // console.log("req", req.body.name);
     const { name, email, password } = req.body;
 
+    //Validation
+
     if (!name || !email || !password) {
         const error = createHttpError(400, "All Fields are required...");
+        return next(error);
+    }
+
+    // Database call
+
+    // const user = await userModel.findOne({ email }); In js we can use this way also if we have the key and value of same name in this case it was "email"
+
+    const user = await userModel.findOne({ email: email });
+
+    if (user) {
+        const error = createHttpError(
+            400,
+            "User already exists with this email..."
+        );
         return next(error);
     }
 
